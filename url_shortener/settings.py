663d129446b9663e417436779bc73d3014858f4e
onorio <onorio@catalpa.io>
2025-03-10 15:10:11 +0900

    prepare for deployment

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-&6c3^%1yc4v@o7_gw_vjg%72qmk)ry&1xa)maj$8)6ogf*k7pc'
-)
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost, 127.0.0.1', cast=lambda v: v.split(','))
 
 
 # Application definition
@@ -148,3 +147,8 @@ FORM_RENDERER = 'django.forms.renderers.DjangoTemplates'
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
